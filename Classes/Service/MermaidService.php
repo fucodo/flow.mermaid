d878ec4c1ffada177ce752ae5b1b7f4ca6a87fac
Kay Strobach <info@kay-strobach.de>
2025-11-05 14:09:33 +0100

    [TASK] harden mermaid service and allow serveral optimizations during the process
    Signed-off-by: Kay Strobach <info@kay-strobach.de>

--- Classes/Service/MermaidService.php
+++ Classes/Service/MermaidService.php
@@ -20,7 +20,13 @@ class MermaidService
      * @Flow\InjectConfiguration(package="fucodo.mermaid", path="puppeteer-config")
      * @var array
      */
-    protected $settings;
+    protected $puppeteerSettings;
+
+    /**
+     * @Flow\InjectConfiguration(package="fucodo.mermaid", path="svgo-config")
+     * @var array
+     */
+    protected $svgoSettings;
 
     /**
      * @Flow\Inject
@@ -34,6 +40,12 @@ class MermaidService
      */
     protected $packageManager;
 
+    /**
+     * @Flow\Inject
+     * @var \Neos\Cache\Frontend\StringFrontend $cache
+     */
+    protected $graphicsCache;
+
     protected function getMermaidPackage(): PackageInterface
     {
         return $this->packageManager->getPackage('fucodo.mermaid');
@@ -44,10 +56,20 @@ class MermaidService
         return FLOW_PATH_DATA . 'Temporary/Mermaid/puppeteer-config.json';
     }
 
+    public function getSvgoConfigPath()
+    {
+        return FLOW_PATH_DATA . 'Temporary/Mermaid/svgo-config.js';
+    }
+
+    public function getMermaidConfigPath(): string
+    {
+        return $this->getMermaidPackage()->getPackagePath() . 'mermaid.config.json';
+    }
+
     protected function preparePuppeteerConfig(): void
     {
         Files::createDirectoryRecursively(dirname($this->getPuppeteerConfigPath()));
-        $config = json_encode($this->settings, JSON_THROW_ON_ERROR);
+        $config = json_encode($this->puppeteerSettings, JSON_THROW_ON_ERROR);
 
         $this->logger->debug('Mermaid puppeteer config', ['config' => $config]);
 
@@ -57,40 +79,118 @@ class MermaidService
         file_put_contents($this->getPuppeteerConfigPath(), $config);
     }
 
-    public function renderMermaidGraph(string $mermaidGraph): string
+    public function renderMermaidGraph(string $mermaidGraph, string $format = 'svg'): string
     {
 
         $file = tempnam(FLOW_PATH_DATA . 'Temporary', 'mermaid-input');
         file_put_contents($file, $mermaidGraph);
-        $buffer = $this->renderMermaidFile($file);
+        $buffer = $this->renderMermaidFile($file, $format);
         unlink($file);
         return $buffer;
     }
 
-    public function renderMermaidFile(string $file): string
+    public function renderMermaidFile(string $file, string $format = 'svg'): string
     {
+        $cacheTag = sha1($format . '-' . file_get_contents($file) . '-' . file_get_contents($this->getPuppeteerConfigPath()) . '-' . file_get_contents($this->getMermaidConfigPath()));
+        if ($this->graphicsCache->has($cacheTag)) {
+            return $this->graphicsCache->get($cacheTag);
+        }
+
+        // prepare stuff
         $this->preparePuppeteerConfig();
         $outputFile = tempnam(FLOW_PATH_DATA . 'Temporary', 'mermaid-output');
-        $outputFileWithExt =  $outputFile . '.svg';
+        $outputFileWithExt =  $outputFile . '.' . $format;
+
+        // render graph
         $command = \sprintf(
-            '%s/node_modules/.bin/mmdc --puppeteerConfigFile %s -e svg -o %s < %s',
+            '%s/node_modules/.bin/mmdc -c %s --puppeteerConfigFile %s -e %s -o %s < %s',
             escapeshellarg($this->getMermaidPackage()->getPackagePath()),
+            escapeshellarg($this->getMermaidConfigPath()),
             escapeshellarg($this->getPuppeteerConfigPath()),
+            escapeshellarg($format),
             escapeshellarg($outputFileWithExt),
             escapeshellarg($file)
         );
-
         $this->logger->debug('Mermaid command', ['command' => $command]);
-
         $process = Process::fromShellCommandline(
             $command
         );
         $process->start();
         $process->wait();
-        $buffer = file_get_contents($outputFile . '.svg');
+
+        // optimize grafics for mpdf etc.
+        #if ($format === 'svg') {
+        #    $this->useInkScape($outputFileWithExt);
+        #    $this->useSvgGo($outputFileWithExt);
+        #}
+
+        // build return stuff
+        $buffer = file_get_contents($outputFileWithExt);
+
+        $this->graphicsCache->set($cacheTag, $buffer);
+
         unlink($outputFile);
         unlink($outputFileWithExt);
         return $buffer;
+    }
+
+    protected function useSvgGo(string $file)
+    {
+        file_put_contents($this->getSvgoConfigPath(), 'module.exports = ' . json_encode($this->svgoSettings, JSON_THROW_ON_ERROR));
+        $command = \sprintf(
+            '%s/node_modules/.bin/svgo --config %s --output %s --input %s',
+            escapeshellarg($this->getMermaidPackage()->getPackagePath()),
+            escapeshellarg($this->getSvgoConfigPath()),
+            escapeshellarg($file),
+            escapeshellarg($file)
+        );
+        $this->logger->debug(
+            'Svgo command',
+            [
+                'command' => $command,
+                'sizeIn' => filesize($file),
+            ]
+        );
+        $process = Process::fromShellCommandline(
+            $command
+        );
+        $process->start();
+        $process->wait();
+        $this->logger->debug(
+            'Svgo command',
+            [
+                'command' => $command, 'output' => $process->getOutput(),
+                'sizeIn' => filesize($file),
+            ]
+        );
+    }
+
+    public function useInkScape(string $file): void
+    {
+        $command = \sprintf(
+            'inkscape --export-filename=%s %s',
+            escapeshellarg($file),
+            escapeshellarg($file)
+        );
+        $this->logger->debug(
+            'inkscape command',
+            [
+                'command' => $command,
+                'sizeIn' => filesize($file),
+            ]
+        );
+        $process = Process::fromShellCommandline(
+            $command
+        );
+        $process->start();
+        $process->wait();
+        $this->logger->debug(
+            'inkscape command',
+            [
+                'command' => $command, 'output' => $process->getOutput(),
+                'sizeIn' => filesize($file),
+            ]
+        );
     }
 
     public function install()
